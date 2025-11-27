f24eb6260e83ca1091227d27708f2026d8dc1a6c
David Nwosu <nwosudavid77@gmail.com>
2025-11-27 08:50:57 +0100

    Remove skip_requesting_account_id from backend.tf
    Removed the 'skip_requesting_account_id' option from the configuration.

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -6,7 +6,6 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    skip_requesting_account_id  = true
     use_path_style              = true
     endpoint                    = "https://us-southeast-1.linodeobjects.com"
   }
