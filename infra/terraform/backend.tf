4b79942ee1c5e3890f3c7bd981f07d85014c88dd
Nwosu David <nwosudavid77@gmail.com>
2025-11-27 09:45:51 +0100

    Update S3 region to us-southeast-1 and add skip_s3_checksum in backend.tf

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -2,13 +2,14 @@ terraform {
   backend "s3" {
     bucket = "todo-terraform-bucket"
     key    = "tf/tfstate"
-    region = "us-east-1"
+    region = "us-southeast-1"
 
     # required for S3-compatible backends
     use_path_style            = true
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
+    skip_s3_checksum = true
 
     endpoints = {
       s3 = "https://us-southeast-1.linodeobjects.com"
