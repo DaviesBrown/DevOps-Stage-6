b75d3561125a9d9c5b27ba9209997c9a2367b6f4
David Nwosu <nwosudavid77@gmail.com>
2025-11-27 08:20:41 +0100

    Update Terraform S3 backend configuration

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,12 +1,13 @@
 terraform {
   backend "s3" {
-    bucket                      = "your-terraform-state-bucket"
+    bucket                      = "todo-terraform-bucket"
     key                         = "tf/tfstate"
     region                      = "us-east-1"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    force_path_style            = true
-    endpoint                    = "us-southeast-1.linodeobjects.com"
+    skip_requesting_account_id  = true
+    use_path_style              = true
+    endpoint                    = "https://us-southeast-1.linodeobjects.com"
   }
 }
