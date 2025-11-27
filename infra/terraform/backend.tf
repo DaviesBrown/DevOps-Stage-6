ee5ed7d5960ca3da8180d2ff4ff56bd95bc90716
David Nwosu <nwosudavid77@gmail.com>
2025-11-27 08:02:59 +0100

    Remove skip_requesting_account_id and skip_s3_checksum
    Removed unnecessary S3 backend configuration options.

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -3,8 +3,6 @@ terraform {
     bucket                      = "todo-terraform-bucket"
     key                         = "tf/tfstate"
     region                      = "us-southeast-1"
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
     endpoints = {
       s3 = "https://us-southeast-1.linodeobjects.com"
     }
