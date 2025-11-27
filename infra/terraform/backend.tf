05d840aeba60120ef4d822a35603aa9641392a03
Nwosu David <nwosudavid77@gmail.com>
2025-11-27 09:05:11 +0100

    Fix incorrect configuration option in Terraform S3 backend

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -5,7 +5,7 @@ terraform {
     region = "us-east-1"
 
     # required for S3-compatible backends
-    force_path_style            = true
+    use_path_style            = true
     skip_region_validation      = true
     skip_credentials_validation = true
 
