e1e47d039b6644b070d718b6582d043841a59423
David Nwosu <nwosudavid77@gmail.com>
2025-11-27 08:14:36 +0100

    Modify Terraform backend configuration
    Updated S3 bucket name and region for Terraform backend configuration.

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,10 +1,12 @@
 terraform {
   backend "s3" {
-    bucket                      = "todo-terraform-bucket"
+    bucket                      = "your-terraform-state-bucket"
     key                         = "tf/tfstate"
-    region                      = "us-southeast"
-    endpoints = {
-      s3 = "https://us-southeast-1.linodeobjects.com"
-    }
+    region                      = "us-east-1"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    force_path_style            = true
+    endpoint                    = "us-southeast-1.linodeobjects.com"
   }
 }
