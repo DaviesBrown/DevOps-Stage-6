f7824133c20c190841efd3f27f756774c321f9c6
Nwosu David <nwosudavid77@gmail.com>
2025-11-27 11:35:36 +0100

    Switch backend from S3 to local in backend.tf

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,20 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket = "todo-terraform-bucket"
-    key    = "tf/tfstate"
-    region = "us-southeast-1"
-    access_key = "2P0YIANJ2BF3SSP0N1P5"
-    secret_key = "0KTXAmMh1QBnqbvKgWbrEHqFerV7usEDHnKDRyc0"
-
-    # required for S3-compatible backends
-    use_path_style            = true
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum = true
-
-    endpoints = {
-      s3 = "https://us-southeast-1.linodeobjects.com"
-    }
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
